a5b4569253b2cd36fb3894f1627b6f4e61de4602
The Tran <the.tran@ext.magnolia-cms.com>
2017-02-21 20:56:46 +0700

    Use youtube search api

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
+import YTSearch from 'youtube-api-search';
+
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 const API_KEY = 'AIzaSyBzzj_xzDX-CYKDKOc5JreUJw-z-OvkVbA';
+
+YTSearch({
+  key: API_KEY,
+  term: 'Nissan GTR'
+}, (data) => {
+  console.log(data);
+});
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
