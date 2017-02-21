a36663b51a79b2cb5876a7b6afb7276a7c7fd65c
The Tran <the.tran@ext.magnolia-cms.com>
2017-02-21 21:28:11 +0700

    Add VideoList component. Use props.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './search_bar';
+import VideoList from './video_list';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: []
+    };
+
+    YTSearch({
+      key: this.props.apiKey,
+      term: 'Nissan GTR'
+    }, (videos) => {
+      console.log(videos);
+
+      this.setState({
+        videos: videos
+      });
+    });
+  }
+
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-import YTSearch from 'youtube-api-search';
-
 import App from './components/app';
 import reducers from './reducers';
 
@@ -12,15 +9,8 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 const API_KEY = 'AIzaSyBzzj_xzDX-CYKDKOc5JreUJw-z-OvkVbA';
 
-YTSearch({
-  key: API_KEY,
-  term: 'Nissan GTR'
-}, (data) => {
-  console.log(data);
-});
-
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <App apiKey={API_KEY} />
   </Provider>
   , document.querySelector('.container'));
